e93e6920570c08f79d8cfbcd48942b84e12cc643
Emmanuel Tavarez <tavareze2003@gmail.com>
2026-04-07 22:00:48 -0400

    Added getter and setter annotation

--- src/main/java/com/tavarlabs/jwtauthpractice/entity/User.java
+++ src/main/java/com/tavarlabs/jwtauthpractice/entity/User.java
@@ -2,8 +2,12 @@ package com.tavarlabs.jwtauthpractice.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "users")
