bfb6e82888286bfab9b1b165a8aadc46bcb8e90b
Emmanuel Tavarez <tavareze2003@gmail.com>
2026-04-07 20:34:52 -0400

    project has been initialized

---
+++ src/main/java/com/tavarlabs/jwtauthpractice/JwtauthpracticeApplication.java
@@ -0,0 +1,13 @@
+package com.tavarlabs.jwtauthpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtauthpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtauthpracticeApplication.class, args);
+	}
+
+}
